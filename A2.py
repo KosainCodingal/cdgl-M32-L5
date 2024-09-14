4ad871607420caff313729fdfe5f851431a3240a
KosainCodingal <kosainabro2@gmail.com>
2024-09-14 18:59:31 +0500

    Activity 2 completed

---
+++ A2.py
@@ -0,0 +1,20 @@
+def paren(s, l, r, p, n):
+    if p == 2*n:
+        for i in s: print(i, end='')
+        print('\n')
+        return
+    
+    if l > r:
+        s[p] = '}'
+        paren(s, l, r+1, p+1, n)
+
+    if l < n:
+        s[p] = '{'
+        paren(s, l+1, r, p+1, n)
+
+
+n = int(input("Enter number of parenthesis: "))
+s = ['']*2*n
+print('\n')
+paren(s, 0, 0, 0, n)
+
